19f3426185a8a7e0c88a72322e3b9e215db7d987
Joseph Barker <jbarker@csumb.edu>
2019-12-11 00:07:52 -0800

    server mod1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/pixiv-clone'));
+app.use(express.static(__dirname + '/dist/PixivClone'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
