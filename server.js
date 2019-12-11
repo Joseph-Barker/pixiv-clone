f54ca2ad7806bf24a58155a10f2d466d8868e34a
Joseph Barker <jbarker@csumb.edu>
2019-12-10 23:06:56 -0800

    fix server conf

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/cst438frontend'));
+app.use(express.static(__dirname + '/dist/pixiv-clone'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/cst438frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/pixiv-clone/index.html'));
 });
 
 // default Heroku PORT
@@ -15,3 +15,4 @@ app.get('/*', function(req, res) {
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 }); 
+
