3b1b576d594a937b177b9cc110a915a8d31f1bdc
Joseph Barker <jbarker@csumb.edu>
2019-12-10 23:53:20 -0800

    remove dis file

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.get('/*', function(req, res) {
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 }); 
-
+//test
