988ebc1351c20f96fc518d6f934bdd3b6f5523c5
Joseph Barker <jbarker@csumb.edu>
2019-12-13 11:19:27 -0800

    added heroku wakeup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  Allimages: Object;
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
+    
+    //call API to wakeup Heroku as soon as possible! 
+    this._http.getAll().subscribe(data => {
+      this.Allimages = data;
+    });  
   }
 
 }
